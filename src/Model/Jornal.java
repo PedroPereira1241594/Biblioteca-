a41a118467e356542f89adfe3e104ee46c579fb3
Francisco <1241587@isep.ipp.pt>
2025-01-31 18:42:43 +0000

    Jornal model
    Livro model
    
    javadoc pronto

--- src/Model/Jornal.java
+++ src/Model/Jornal.java
@@ -3,6 +3,10 @@ package Model;
 import java.time.LocalDate;
 import java.util.Objects;
 
+/**
+ * Classe que representa um Jornal.
+ * Cada jornal é identificado pelo seu ISSN.
+ */
 public class Jornal extends ItemEmprestavel {
     private String titulo;
     private String editora;
@@ -10,7 +14,15 @@ public class Jornal extends ItemEmprestavel {
     private String issn;
     private LocalDate dataPublicacao;
 
-    // Construtor
+    /**
+     * Construtor da classe Jornal.
+     *
+     * @param issn Identificador único do jornal.
+     * @param titulo Título do jornal.
+     * @param categoria Categoria do jornal.
+     * @param editora Editora responsável pela publicação do jornal.
+     * @param dataPublicacao Data de publicação do jornal.
+     */
     public Jornal(String issn, String titulo, String categoria, String editora, LocalDate dataPublicacao) {
         super(issn);
         this.titulo = titulo;
@@ -20,54 +32,111 @@ public class Jornal extends ItemEmprestavel {
         this.issn = issn;
     }
 
-    // Getters
+    /**
+     * Obtém o título do jornal.
+     *
+     * @return O título do jornal.
+     */
     public String getTitulo() {
         return titulo;
     }
 
+    /**
+     * Obtém a editora do jornal.
+     *
+     * @return A editora do jornal.
+     */
     public String getEditora() {
         return editora;
     }
 
+    /**
+     * Obtém a categoria do jornal.
+     *
+     * @return A categoria do jornal.
+     */
     public String getCategoria() {
         return categoria;
     }
 
+    /**
+     * Obtém o ISSN do jornal.
+     *
+     * @return O ISSN do jornal.
+     */
     public String getIssn() {
         return issn;
     }
 
+    /**
+     * Obtém a data de publicação do jornal.
+     *
+     * @return A data de publicação do jornal.
+     */
     public LocalDate getDataPublicacao() {
         return dataPublicacao;
     }
 
-    // Setters
+    /**
+     * Define o título do jornal.
+     *
+     * @param titulo O título a ser definido.
+     */
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
 
+    /**
+     * Define a editora do jornal.
+     *
+     * @param editora A editora a ser definida.
+     */
     public void setEditora(String editora) {
         this.editora = editora;
     }
 
+    /**
+     * Define a categoria do jornal.
+     *
+     * @param categoria A categoria a ser definida.
+     */
     public void setCategoria(String categoria) {
         this.categoria = categoria;
     }
 
+    /**
+     * Define o ISSN do jornal.
+     *
+     * @param issn O ISSN a ser definido.
+     */
     public void setIssn(String issn) {
         this.issn = issn;
     }
 
+    /**
+     * Define a data de publicação do jornal.
+     *
+     * @param dataPublicacao A data de publicação a ser definida.
+     */
     public void setDataPublicacao(LocalDate dataPublicacao) {
         this.dataPublicacao = dataPublicacao;
     }
 
+    /**
+     * Obtém o identificador único do jornal, que é o ISSN.
+     *
+     * @return O ISSN do jornal.
+     */
     @Override
     public String getIdentificador() {
-        return issn;  // Retorna o ISSN como identificador único
+        return issn;
     }
 
-    // Método toString adaptado para Jornal
+    /**
+     * Retorna uma representação em String do objeto Jornal.
+     *
+     * @return Uma string formatada com as informações do jornal.
+     */
     @Override
     public String toString() {
         return "Jornal {" +
@@ -79,7 +148,12 @@ public class Jornal extends ItemEmprestavel {
                 '}';
     }
 
-    // Implementação de equals para comparar jornais com base no ISSN
+    /**
+     * Compara este jornal com outro objeto para verificar se são iguais.
+     *
+     * @param obj O objeto a ser comparado.
+     * @return true se os objetos forem iguais, false caso contrário.
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
@@ -89,12 +163,17 @@ public class Jornal extends ItemEmprestavel {
             return false;
         }
         Jornal jornal = (Jornal) obj;
-        return Objects.equals(issn, jornal.issn);  // Compara pelo ISSN
+        return Objects.equals(issn, jornal.issn);
     }
 
-    // Implementação de hashCode com base no ISSN
+    /**
+     * Retorna o código hash do jornal baseado no ISSN.
+     *
+     * @return O código hash do jornal.
+     */
     @Override
     public int hashCode() {
-        return Objects.hash(issn);  // Gera hash code com base no ISSN
+        return Objects.hash(issn);
     }
 }
+
--- src/Model/Livro.java
+++ src/Model/Livro.java
@@ -2,6 +2,10 @@ package Model;
 
 import java.util.Objects;
 
+/**
+ * Classe que representa um livro.
+ * Herda de ItemEmprestavel e possui atributos como nome, editora, categoria, ano, autor e ISBN.
+ */
 public class Livro extends ItemEmprestavel {
     private String nome;
     private String editora;
@@ -10,6 +14,16 @@ public class Livro extends ItemEmprestavel {
     private String autor;
     private String isbn;
 
+    /**
+     * Construtor da classe Livro.
+     *
+     * @param nome      Nome do livro.
+     * @param editora   Nome da editora.
+     * @param categoria Categoria do livro.
+     * @param ano       Ano de publicação.
+     * @param autor     Nome do autor.
+     * @param isbn      Código ISBN do livro.
+     */
     public Livro(String nome, String editora, String categoria, int ano, String autor, String isbn) {
         super(isbn);
         this.nome = nome;
@@ -20,55 +34,119 @@ public class Livro extends ItemEmprestavel {
         this.isbn = isbn;
     }
 
+    /**
+     * Obtém o nome do livro.
+     *
+     * @return Nome do livro.
+     */
     public String getNome() {
         return nome;
     }
 
+    /**
+     * Define o nome do livro.
+     *
+     * @param nome Nome do livro.
+     */
     public void setNome(String nome) {
         this.nome = nome;
     }
 
+    /**
+     * Obtém a editora do livro.
+     *
+     * @return Nome da editora.
+     */
     public String getEditora() {
         return editora;
     }
 
+    /**
+     * Define a editora do livro.
+     *
+     * @param editora Nome da editora.
+     */
     public void setEditora(String editora) {
         this.editora = editora;
     }
 
+    /**
+     * Obtém a categoria do livro.
+     *
+     * @return Categoria do livro.
+     */
     public String getCategoria() {
         return categoria;
     }
 
+    /**
+     * Define a categoria do livro.
+     *
+     * @param categoria Categoria do livro.
+     */
     public void setCategoria(String categoria) {
         this.categoria = categoria;
     }
 
+    /**
+     * Obtém o ano de publicação do livro.
+     *
+     * @return Ano de publicação.
+     */
     public int getAno() {
         return ano;
     }
 
+    /**
+     * Define o ano de publicação do livro.
+     *
+     * @param ano Ano de publicação.
+     */
     public void setAno(int ano) {
         this.ano = ano;
     }
 
+    /**
+     * Obtém o nome do autor do livro.
+     *
+     * @return Nome do autor.
+     */
     public String getAutor() {
         return autor;
     }
 
+    /**
+     * Define o nome do autor do livro.
+     *
+     * @param autor Nome do autor.
+     */
     public void setAutor(String autor) {
         this.autor = autor;
     }
 
+    /**
+     * Obtém o código ISBN do livro.
+     *
+     * @return Código ISBN.
+     */
     public String getIsbn() {
         return isbn;
     }
 
+    /**
+     * Define o código ISBN do livro.
+     *
+     * @param isbn Código ISBN.
+     */
     public void setIsbn(String isbn) {
         this.isbn = isbn;
     }
 
-    // Método toString adaptado para Livro
+    /**
+     * Retorna uma representação textual do livro.
+     *
+     * @return String com os detalhes do livro.
+     */
     @Override
     public String toString() {
         return "Livro: " + nome + "\n" +
@@ -79,7 +157,12 @@ public class Livro extends ItemEmprestavel {
                 "ISBN: " + isbn;
     }
 
-    // Implementação de equals para comparar livros com base no ISBN
+    /**
+     * Compara dois livros com base no ISBN.
+     *
+     * @param obj Objeto a ser comparado.
+     * @return true se os ISBNs forem iguais, caso contrário, false.
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
@@ -89,18 +172,26 @@ public class Livro extends ItemEmprestavel {
             return false;
         }
         Livro livro = (Livro) obj;
-        return Objects.equals(isbn, livro.isbn);  // Compara pelo ISBN
+        return Objects.equals(isbn, livro.isbn);
     }
 
-    // Implementação de hashCode com base no ISBN
+    /**
+     * Retorna o código hash do livro, baseado no ISBN.
+     *
+     * @return Código hash do livro.
+     */
     @Override
     public int hashCode() {
-        return Objects.hash(isbn);  // Gera hash code com base no ISBN
+        return Objects.hash(isbn);
     }
 
-    // Implementação do método getIdentificador() da classe ItemEmprestavel
+    /**
+     * Retorna o identificador único do livro (ISBN).
+     *
+     * @return Código ISBN do livro.
+     */
     @Override
     public String getIdentificador() {
-        return isbn;  // Retorna o ISBN como identificador único
+        return isbn;
     }
 }
